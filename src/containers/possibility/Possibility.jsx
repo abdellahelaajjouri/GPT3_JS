708086a67814278bffe58c152c32e8d2fa66c5a9
abdellahelaajjouri <abdellah.elaajjouri@gmail.com>
2022-11-25 16:51:22 +0100

    'Only-Footer-Component-left'

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -7,7 +7,7 @@ function Possibility() {
       <div className="gpt3__possibility-image">
         <img src={PossibilityImage} alt="possibility" />
       </div>
-      <div className="gpt3__possi  bility-content">
+      <div className="gpt3__possibility-content">
         <h4>Request Early Access to Get Started</h4>
         <h1 className="gradient__text">
           The possibilities are beyond your imagination
